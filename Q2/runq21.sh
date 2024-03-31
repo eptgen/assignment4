ef62bd9bcbdc98e4ba59971935cb52ea6b3011a8
23jsr7 <evolene@cs.williams.edu>
2024-03-31 12:40:13 -0400

    q21

---
+++ Q2/runq21.sh
@@ -0,0 +1,6 @@
+python Q21_image_optimization.py --prompt="a standing corgi dog" --sds_guidance=0
+python Q21_image_optimization.py --prompt="a standing corgi dog" --sds_guidance=1 --postfix="_guidance"
+python Q21_image_optimization.py --prompt="cat playing computer games" --sds_guidance=0
+python Q21_image_optimization.py --prompt="cat playing computer games" --sds_guidance=1 --postfix="_guidance"
+python Q21_image_optimization.py --prompt="a happy cow" --sds_guidance=0
+python Q21_image_optimization.py --prompt="a happy cow" --sds_guidance=1 --postfix="_guidance"
