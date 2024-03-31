8b1b3edd3125c90b84931fc5502f146ec2537096
23jsr7 <evolene@cs.williams.edu>
2024-03-31 00:19:55 -0400

    camera

--- Q2/Q22_mesh_optimization.py
+++ Q2/Q22_mesh_optimization.py
@@ -110,7 +110,8 @@ def optimize_mesh_texture(
 
         # Forward pass
         # Render a randomly sampled camera view to optimize in this iteration
-        rend = renderer(mesh, cameras = cameras, lights = lights)[0, ..., :3]
+        random_camera = query_cameras[int(random() * len(query_cameras))]
+        rend = renderer(mesh, cameras = random_camera, lights = lights)[0, ..., :3]
         # Encode the rendered image to latents
         latents = sds.encode_imgs(torch.permute(rend, (2, 0, 1)).unsqueeze(0))
         # Compute the loss
