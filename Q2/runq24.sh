1d9581e31033395ebbf29645e4fcb25276d2eb90
23jsr7 <evolene@cs.williams.edu>
2024-04-02 12:10:18 -0400

    q24

---
+++ Q2/runq24.sh
@@ -0,0 +1,2 @@
+python Q23_nerf_optimization.py --prompt="a standing corgi dog" --iters=4100 --view_dep_text=1 --lambda_entropy=0.0005 --lambda_orient=0.01 --latent_iter_ratio=0.2 --postfix="_viewdep"
+python Q23_nerf_optimization.py --prompt="cat playing computer games" --iters=4100 --view_dep_text=1 --lambda_entropy=0.0005 --lambda_orient=0.01 --latent_iter_ratio=0.2 --postfix="_viewdep"
